a8f470cf7c2ba11d3deb075abf154861f4e5fade
Forhadul Islam <mdforhadul44@gmail.com>
2026-04-14 00:51:01 +0600

    addd alllll

--- app/layout.tsx
+++ app/layout.tsx
@@ -398,6 +398,35 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Meta Pixel Code */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1427866301708580');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1427866301708580&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         {/* Microsoft Clarity tracking script */}
         <Script
           id="microsoft-clarity"
