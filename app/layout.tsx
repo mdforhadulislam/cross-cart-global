e81110d4426f2805f2b3561f990c0fba1acacb34
Forhadul Islam <mdforhadul44@gmail.com>
2026-04-14 11:23:09 +0600

    add alll

--- app/layout.tsx
+++ app/layout.tsx
@@ -400,6 +400,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+
+<Script
+  id="facebook-pixel"
+  strategy="lazyOnload" // 🔥 better than afterInteractive
+>
+  {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}
+    (window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '26093014930391502');
+  `}
+</Script>
         {/* Microsoft Clarity tracking script */}
         <Script
           id="microsoft-clarity"
--- components/FacebookPixel.tsx
+++ components/FacebookPixel.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import { pageview } from "@/lib/fpixel";
 
 export default function FacebookPixel() {
   const pathname = usePathname();
 
   useEffect(() => {
-    pageview();
+    const timeout = setTimeout(() => {
+      pageview();
+    }, 500); // small delay ensures fbq loaded
+
+    return () => clearTimeout(timeout);
   }, [pathname]);
 
   return null;
