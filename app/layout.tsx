0c40e831cc7989155ccb2ccb0072418cb78bf87c
Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2026-05-04 12:46:29 +0600

    Integrate Google Tag Manager into layout
    Added Google Tag Manager scripts for tracking.

--- app/layout.tsx
+++ app/layout.tsx
@@ -633,6 +633,16 @@ export default function RootLayout({
           }}
         />
 
+         <Script id="gtm-script" strategy="afterInteractive">
+  {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TDP9QDWF');
+  `}
+</Script>
+
         {/* Google Analytics Script */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-MBRH1VJHZP"
@@ -657,6 +667,15 @@ export default function RootLayout({
       <body
         className={` ${keaniaOne.variable} ${keaniaOne.variable} antialiased`}
       >
+           {/* Google Tag Manager (noscript) */}
+  <noscript>
+    <iframe
+      src="https://www.googletagmanager.com/ns.html?id=GTM-TDP9QDWF"
+      height="0"
+      width="0"
+      style={{ display: "none", visibility: "hidden" }}
+    />
+  </noscript>
         <Script
           id="organization-schema"
           type="application/ld+json"
@@ -677,6 +696,7 @@ export default function RootLayout({
     __html: JSON.stringify(localBusinessSchema),
   }}
 />
+         
 
         <TooltipProvider>
           <Suspense fallback={null}>
