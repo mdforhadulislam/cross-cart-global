99d1ff193901737ab1ea4737d2945fa9ce5e37d4
Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2026-05-06 17:54:46 +0600

    Integrate Clarity tracking script in layout
    Added Clarity script for tracking and analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -600,6 +600,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+
+               <Script
+        id="clarity-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "w8fn60boix");
+          `,
+        }}
+      />
+
+         
         <Script
           id="facebook-pixel"
           strategy="lazyOnload" // 🔥 better than afterInteractive
